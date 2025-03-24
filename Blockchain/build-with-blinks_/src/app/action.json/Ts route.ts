5dce4d0b7b85965e108335c151d4116046fac6d5
AlexIchie-coder <alexnnadi7@gmail.com>
2025-03-24 10:16:36 +0100

    BlockChain

---
+++ Blockchain/build-with-blinks_/src/app/action.json/Ts route.ts
@@ -0,0 +1,8 @@
+export const GET = async () => {
+    const payload: ActionsJson = {
+        rules: [
+            // map all root routes to an action
+            
+        ]
+    }
+}
---
+++ Blockchain/build-with-blinks_/src/app/api/actions/memo/Ts route.ts
@@ -0,0 +1,64 @@
+import {ACTIONS_CORS_HEADERS, BLOCKCHAIN_IDS} from '@solana/actions';
+
+// CAIP-2 blockchain ID of Solana Devnet
+const blockchain
+
+// headers for action 
+const headers
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export const Post = async (reg: Request) => {
+    const body: ActionPostRequest <{memoTest: string}> = awaits reg.json();
+    const acccount = new PublicKeyCredential(body.account);
+
+    const memoText = body.data!.memoText as string;
+
+    const msg: new TransactionMessage({   Parsing error: '('getExpectedRequestStore.
+        payerKey: acccount,
+        recentBlockhash: (await connection.getlatestBlockhash()).blockhash,
+        instruction: [
+            new TractionInstruction({
+                programId: new PublicKeyCredential(MEMO_PROGRAM_ID),
+            })
+        ]
+    })
+}
